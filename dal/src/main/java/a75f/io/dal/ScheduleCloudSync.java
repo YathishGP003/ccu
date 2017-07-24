04b922ccb9d32db34583e4238e2143140a3cb32a
Ryan Mattison <ryanmattison@75Fs-MacBook-Pro-2.local>
2017-07-24 10:50:57 -0500

    Get rid of error

--- dal/src/main/java/a75f/io/dal/ScheduleCloudSync.java
+++ dal/src/main/java/a75f/io/dal/ScheduleCloudSync.java
@@ -1,6 +1,9 @@
 package a75f.io.dal;
 
 import com.kinvey.android.store.DataStore;
+import com.kinvey.android.sync.KinveyPullResponse;
+import com.kinvey.android.sync.KinveyPushResponse;
+import com.kinvey.android.sync.KinveySyncCallback;
 import com.kinvey.java.store.StoreType;
 
 import java.io.IOException;
@@ -16,7 +19,38 @@ public class ScheduleCloudSync {
 
     public static boolean saveSchedule(Schedule schedule) throws IOException {
         DataStore<Schedule> scheduleDataStore = DataStore.collection(Schedule.class.getSimpleName(), Schedule.class, StoreType.NETWORK, Globals.getInstance().getKinveyClient());
-        scheduleDataStore.sync(schedule);
+
+        /*scheduleDataStore.sync(new KinveySyncCallback<Schedule>() {
+            @Override
+            public void onSuccess(KinveyPushResponse kinveyPushResponse, KinveyPullResponse<Schedule> kinveyPullResponse) {
+
+            }
+
+            @Override
+            public void onPullStarted() {
+
+            }
+
+            @Override
+            public void onPushStarted() {
+
+            }
+
+            @Override
+            public void onPullSuccess(KinveyPullResponse<Schedule> kinveyPullResponse) {
+
+            }
+
+            @Override
+            public void onPushSuccess(KinveyPushResponse kinveyPushResponse) {
+
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+
+            }
+        });*/
         return true;
     }
 

d253b1b03e1f94aee6763413e7b70505068d7ecc
Ryan HOME PC <ryan@75f.io>
2017-08-14 16:57:52 -0500

    Fixed regular update message

--- bo/src/main/java/a75f/io/bo/serial/WrmToCmOverAirWrmRegularUpdateMessage_t.java
+++ bo/src/main/java/a75f/io/bo/serial/WrmToCmOverAirWrmRegularUpdateMessage_t.java
@@ -30,33 +30,29 @@ public class WrmToCmOverAirWrmRegularUpdateMessage_t extends Struct
 	public final Unsigned8 measuredMotor1ForwardRpm = new Unsigned8(); // in 1/10s of rpm
 	
 	public final Unsigned8 measuredMotor1ReverseRpm = new Unsigned8(); // in 1/10s of rpm
-	public final Unsigned16 airflow2Temperature = new Unsigned16(); // airflow temp in 1/10 F // additional bytes to make legacy structure as new_protocol structure
-	public final Unsigned16 extAnalog2Sense = new Unsigned16(); // external voltage sense in mv units // additional bytes to make legacy structure as new_protocol structure
-	public final Unsigned16 measuredMotor2ForwardRpm = new Unsigned16(); // in 1/10s of rpm // additional bytes to make legacy structure as new_protocol structure
-	public final Unsigned8 measuredMotor2ReverseRpm = new Unsigned8(); // in 1/10s of rpm // additional bytes to make legacy structure as new_protocol structure
 	
-	public class Status extends Struct
-	{
-		
-		public final Unsigned16 batteryStatus = new Unsigned16(2);// 00 - failsafe mode( outline ), 01 33% full * 1 bar ) , 10 100% full ( 2 bars )
-		
-		public final Unsigned16 extDigital1 = new Unsigned16(1); // occupancy mode from WRM is no longer supported
-		
-		public final Unsigned16 extDigital2 = new Unsigned16(1); // instead those 2 bits are now used for digital input
-		
-		public final Unsigned16 occupancyStatus = new Unsigned16(1); // 1 is occupied, 0 is unoccupied
-		
-		public final Unsigned16 conditioningMode = new Unsigned16(1); // 1 is heating, 0 is cooling
-		
-		public final Unsigned16 externalPower = new Unsigned16(1); // 0 is battery power, 1 is external DC power
-		
-		public final Unsigned16 nodeType = new Unsigned16(1); // 0 is end node, 1 is router
-		
-		public final Unsigned16 motorJammed = new Unsigned16(1); // 0 is motor ok, 1 is motor error
-		
-		public final Unsigned16 updatedOccupiedTemp = new Unsigned16(1); // 1 is occupied set temp was changed from WRM. this is set in async update packet sent after user interaction
-		
-		public final Unsigned16 reserved = new Unsigned16(6);
-	}
+	public final Unsigned16 batteryStatus = new Unsigned16(2);// 00 - failsafe mode( outline ), 01 33% full * 1 bar ) , 10 100% full ( 2 bars )
+	
+	public final Unsigned16 extDigital1 = new Unsigned16(1); // occupancy mode from WRM is no longer supported
+	
+	public final Unsigned16 extDigital2 = new Unsigned16(1); // instead those 2 bits are now used for digital input
+	
+	public final Unsigned16 occupancyStatus = new Unsigned16(1); // 1 is occupied, 0 is unoccupied
+	
+	public final Unsigned16 conditioningMode = new Unsigned16(1); // 1 is heating, 0 is cooling
+	
+	public final Unsigned16 externalPower = new Unsigned16(1); // 0 is battery power, 1 is external DC power
+	
+	public final Unsigned16 nodeType = new Unsigned16(1); // 0 is end node, 1 is router
+	
+	public final Unsigned16 motorJammed = new Unsigned16(1); // 0 is motor ok, 1 is motor error
+	
+	public final Unsigned16 updatedOccupiedTemp = new Unsigned16(1); // 1 is occupied set temp was changed from WRM. this is set in async update packet sent after user interaction
+	
+	public final Unsigned16 reserved                 = new Unsigned16(6);
+	public final Unsigned16 airflow2Temperature      = new Unsigned16(); // airflow temp in 1/10 F // additional bytes to make legacy structure as new_protocol structure
+	public final Unsigned16 extAnalog2Sense          = new Unsigned16(); // external voltage sense in mv units // additional bytes to make legacy structure as new_protocol structure
+	public final Unsigned16 measuredMotor2ForwardRpm = new Unsigned16(); // in 1/10s of rpm // additional bytes to make legacy structure as new_protocol structure
+	public final Unsigned8  measuredMotor2ReverseRpm = new Unsigned8(); // in 1/10s of rpm // additional bytes to make legacy structure as new_protocol structure
 }
 

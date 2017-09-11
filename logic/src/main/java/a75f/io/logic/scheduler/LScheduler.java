b918605a21e414cd3f3672a241be061ec25aba44
Ryan Mattison <ryan@75f.io>
2017-09-11 12:42:57 -0500

    Logging

--- logic/src/main/java/a75f/io/logic/scheduler/LScheduler.java
+++ logic/src/main/java/a75f/io/logic/scheduler/LScheduler.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -138,6 +139,7 @@ public class LScheduler
 		public void onReceive(Context context, Intent intent)
 		{
 			//Remove mCurrentScheduledItem, Notify LZoneProfile.
+            Log.i("LScheduler", "ON recieve scheduled event");
 			SortedSet<Map.Entry<UUID, ScheduledItem>> entries =
 					mapOfEntriesSortedByTimeStamp(mScheduledItems);
 			ArrayList<ScheduledItem> removedScheduledItems = new ArrayList<>();

50ea18e8c7e901fabc1ff8f5855f08fcd6f02511
mahesh75f <pvmahesh@75f.io>
2020-03-17 19:59:01 +0530

    resolved RENATUS-997

--- app/src/main/java/a75f/io/renatus/util/ActivityLifecycleHandler.java
+++ app/src/main/java/a75f/io/renatus/util/ActivityLifecycleHandler.java
@@ -50,6 +50,7 @@ public class ActivityLifecycleHandler implements Application.ActivityLifecycleCa
     @Override
     public void onActivityDestroyed(Activity activity) {
         prefs.setBoolean("APP_START", true);
+        prefs.setBoolean("registered", false);
         AlertManager.getInstance().clearAlertsWhenAppClose();
     }
 }

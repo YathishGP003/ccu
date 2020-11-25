65eb2904191b85649155f9ba093f233992a9329e
mahesh75f <pvmahesh@75f.io>
2020-11-25 15:38:16 +0530

    update type

--- app/src/main/java/a75f/io/renatus/modbus/ZoneRecyclerModbusParamAdapter.java
+++ app/src/main/java/a75f/io/renatus/modbus/ZoneRecyclerModbusParamAdapter.java
@@ -55,7 +55,7 @@ public class ZoneRecyclerModbusParamAdapter extends RecyclerView.Adapter<ZoneRec
                 case "decimal":
                 case "long":
                 case "binary":
-                case "int":
+                case "integer":
                 case "boolean":
                     if (modbusParam.get(position).getUserIntentPointTags() != null && modbusParam.get(position).getUserIntentPointTags().size() > 0) {
                         viewHolder.spValue.setVisibility(View.VISIBLE);

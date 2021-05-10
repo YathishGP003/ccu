2123dfa7d7765afced1c16a89c775414c706334e
Samjith Sadasivan <samjith@75f.io>
2021-05-10 10:50:49 -0500

    Handle smartstat profile pubnub updates

--- logic/src/main/java/a75f/io/logic/pubnub/StandaloneConfigHandler.java
+++ logic/src/main/java/a75f/io/logic/pubnub/StandaloneConfigHandler.java
@@ -1,5 +1,7 @@
 package a75f.io.logic.pubnub;
 
+import android.util.Log;
+
 import com.google.gson.JsonObject;
 
 import a75f.io.api.haystack.CCUHsApi;
@@ -11,13 +13,18 @@ import a75f.io.logic.L;
 import a75f.io.logic.bo.building.ss2pfcu.FanCoilUnitUtil;
 import a75f.io.logic.bo.building.sscpu.ConventionalPackageUnitUtil;
 import a75f.io.logic.bo.building.sshpu.HeatPumpPackageUnitUtil;
+import a75f.io.logic.jobs.ScheduleProcessJob;
 
 public class StandaloneConfigHandler {
     
     public static void updateConfigPoint(JsonObject msgObject, Point configPoint, CCUHsApi hayStack) {
         CcuLog.i(L.TAG_CCU_PUBNUB, "updateStandaloneConfigPoint " + msgObject.toString());
-        
-        if (configPoint.getMarkers().contains(Tags.FCU)) {
+    
+        if (configPoint.getMarkers().contains(Tags.DESIRED)) {
+            ScheduleProcessJob.handleDesiredTempUpdate(configPoint, false, 0);
+        } else if (configPoint.getMarkers().contains(Tags.SCHEDULE_TYPE)) {
+            ScheduleProcessJob.handleScheduleTypeUpdate(configPoint);
+        } else if (configPoint.getMarkers().contains(Tags.FCU)) {
             FanCoilUnitUtil.updateFCUProfile(configPoint, msgObject, hayStack);
         } else if (HSUtil.isCPUEquip(configPoint.getId(), hayStack)) {
             //CPU config points do not seem to have 'cpu' tag. Hence checking the equip type to identify profile.

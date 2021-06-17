efd7de8174e5fe9733cbdf657d58e03cef375fc8
aniket <aniketkumar2808@gmail.com>
2021-06-17 20:07:12 +0530

    While selecting or changing the System profile,the relay mapping will disappear till the loader is shown. Once loader gets off,correct relay mapping will be shown.

--- app/src/main/java/a75f/io/renatus/TempOverrideFragment.java
+++ app/src/main/java/a75f/io/renatus/TempOverrideFragment.java
@@ -4,16 +4,25 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import a75f.io.api.haystack.Equip;
+import a75f.io.logic.bo.building.dualduct.DualDuctUtil;
+import a75f.io.logic.jobs.ScheduleProcessJob;
 import butterknife.ButterKnife;
 
 /**
  * Created by Mahesh on 18-07-2019.
  */
 public class TempOverrideFragment extends Fragment {
+    ArrayList<HashMap> openZoneMap;
 
     public TempOverrideFragment() {
 
@@ -35,5 +44,56 @@ public class TempOverrideFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        /*for(int k=0;k<openZoneMap.size();k++) {
+            Equip updatedEquip = new Equip.Builder().setHashMap(openZoneMap.get(k)).build();
+            if (updatedEquip.getProfile().startsWith("DAB")) {
+                HashMap dabPoints = ScheduleProcessJob.getDABEquipPoints(updatedEquip.getId());
+                Log.i("PointsValue", "DAB Points:" + dabPoints.toString());
+                loadDABPointsUI(dabPoints, inflater, linearLayoutZonePoints, updatedEquip.getGroup());
+            }
+            if (updatedEquip.getProfile().startsWith("VAV")) {
+                HashMap vavPoints = ScheduleProcessJob.getVAVEquipPoints(updatedEquip.getId());
+                Log.i("PointsValue", "VAV Points:" + vavPoints.toString());
+                loadVAVPointsUI(vavPoints, inflater, linearLayoutZonePoints, updatedEquip.getGroup());
+            }
+            if (updatedEquip.getProfile().startsWith("SSE")) {
+                HashMap ssePoints = ScheduleProcessJob.getSSEEquipPoints(updatedEquip.getId());
+                Log.i("PointsValue", "SSE Points:" + ssePoints.toString());
+                loadSSEPointsUI(ssePoints, inflater, linearLayoutZonePoints, updatedEquip.getGroup());
+            }
+            if (updatedEquip.getProfile().startsWith("TEMP_INFLUENCE")) {
+                HashMap tiPoints = ScheduleProcessJob.getTIEquipPoints(updatedEquip.getId());
+                Log.i("PointsValue", "TI Points:" + tiPoints.toString());
+                loadTIPointsUI(tiPoints, inflater, linearLayoutZonePoints, updatedEquip.getGroup());
+            }
+            if (updatedEquip.getProfile().startsWith("SMARTSTAT_TWO_PIPE_FCU")) {
+                HashMap p2FCUPoints = ScheduleProcessJob.get2PFCUEquipPoints(updatedEquip.getId());
+                Log.i("PointsValue", "2PFCU Points:" + p2FCUPoints.toString());
+                loadSS2PFCUPointsUI(p2FCUPoints, inflater, linearLayoutZonePoints, equipId, true, updatedEquip.getGroup());
+
+            }
+            if (updatedEquip.getProfile().startsWith("SMARTSTAT_FOUR_PIPE_FCU")) {
+                HashMap p4FCUPoints = ScheduleProcessJob.get4PFCUEquipPoints(updatedEquip.getId());
+                Log.i("PointsValue", "4PFCU Points:" + p4FCUPoints.toString());
+                loadSS4PFCUPointsUI(p4FCUPoints, inflater, linearLayoutZonePoints, equipId, true, updatedEquip.getGroup());
+            }
+            if (updatedEquip.getProfile().startsWith("SMARTSTAT_CONVENTIONAL_PACK_UNIT")) {
+                HashMap cpuEquipPoints = ScheduleProcessJob.getCPUEquipPoints(updatedEquip.getId());
+                Log.i("PointsValue", "CPU Points:" + cpuEquipPoints.toString());
+                loadSSCPUPointsUI(cpuEquipPoints, inflater, linearLayoutZonePoints, updatedEquip.getId(), true, updatedEquip.getGroup(),false);
+                //isCPUloaded = true;
+            }
+            if (updatedEquip.getProfile().startsWith("SMARTSTAT_HEAT_PUMP_UNIT")) {
+                HashMap hpuEquipPoints = ScheduleProcessJob.getHPUEquipPoints(updatedEquip.getId());
+                Log.i("PointsValue", "HPU Points:" + hpuEquipPoints.toString());
+                loadSSHPUPointsUI(hpuEquipPoints, inflater, linearLayoutZonePoints, updatedEquip.getId(), true, updatedEquip.getGroup(),false);
+                //isHPUloaded = true;
+            }
+            if (updatedEquip.getProfile().startsWith("DUAL_DUCT")) {
+                HashMap dualDuctPoints = DualDuctUtil.getEquipPointsForView(updatedEquip.getId());
+                Log.i("PointsValue", "DUAL_DUCT Points:" + dualDuctPoints.toString());
+                loadDualDuctPointsUI(dualDuctPoints, inflater, linearLayoutZonePoints, updatedEquip.getGroup());
+            }
+        }*/
     }
 }

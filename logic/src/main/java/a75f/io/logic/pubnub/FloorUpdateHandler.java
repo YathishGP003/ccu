ba1837f2554b90849ca42437963427408d2df4b4
JayaTheertha <jtheertha@75f.io>
2021-08-09 15:09:34 +0530

    FloorNum and orientation pubnub

--- logic/src/main/java/a75f/io/logic/pubnub/FloorUpdateHandler.java
+++ logic/src/main/java/a75f/io/logic/pubnub/FloorUpdateHandler.java
@@ -1,9 +1,6 @@
 package a75f.io.logic.pubnub;
 
-import android.util.Log;
-
 import com.google.gson.JsonObject;
-
 import org.projecthaystack.HDict;
 import org.projecthaystack.HDictBuilder;
 import org.projecthaystack.HGridBuilder;
@@ -11,9 +8,8 @@ import org.projecthaystack.HRef;
 import org.projecthaystack.HRow;
 import org.projecthaystack.io.HZincReader;
 import org.projecthaystack.io.HZincWriter;
-
+import java.util.HashMap;
 import java.util.Iterator;
-
 import a75f.io.api.haystack.CCUHsApi;
 import a75f.io.api.haystack.Floor;
 import a75f.io.api.haystack.sync.HttpUtil;
@@ -22,38 +18,28 @@ public class FloorUpdateHandler {
     public static final String CMD = "updateEntity";
     public static void updateFloor(JsonObject msgObject){
         String uid = msgObject.get("ids").getAsJsonArray().get(0).toString().replaceAll("\"", "");
+        HashMap entity = CCUHsApi.getInstance().read("id == " + HRef.make(uid));
+        if (entity.get("floor") == null) {
+            return;
+        }
         HDictBuilder b = new HDictBuilder().add("id", HRef.copy(uid));
         HDict[] dictArr  = {b.toDict()};
         String response = HttpUtil.executePost(CCUHsApi.getInstance().getHSUrl() + "read",
                 HZincWriter.gridToString(HGridBuilder.dictsToGrid(dictArr)));
-        Log.i("Jayatheertha",CCUHsApi.getInstance().getHSUrl());
-        Log.i("Jayatheertha",uid);
-        Log.i("Jayatheertha",response);
-        Floor floor = null;
         if (response != null) {
-            HZincReader zReader = new HZincReader(response);
-            Iterator it = zReader.readGrid().iterator();
-            while (it.hasNext()) {
-                HRow row = (HRow) it.next();
-                String id = row.get("id").toString();
-                String dis = row.get("dis").toString();
-                String floorStr = row.get("floor").toString();
-                String siteRef = row.get("siteRef").toString();
-                String orientation = row.get("orientation").toString();
-                String floorNum = row.get("floorNum").toString();
-                /*floor = new Floor.Builder()
+            HZincReader hZincReader = new HZincReader(response);
+            Iterator hZincReaderIterator = hZincReader.readGrid().iterator();
+            while (hZincReaderIterator.hasNext()) {
+                HRow row = (HRow) hZincReaderIterator.next();
+                Floor floor = new Floor.Builder()
                         .setDisplayName(row.get("dis").toString())
                         .setSiteRef(row.get("siteRef").toString())
-                        .build();*/
-                //floor.setId(row.get("id").toString());
+                        .build();
+                floor.setId(row.get("id").toString());
                 floor.setOrientation(Double.parseDouble(row.get("orientation").toString()));
                 floor.setFloorNum(Double.parseDouble(row.get("floorNum").toString()));
-
-                Log.i("Jayatheertha", id+">>>"+dis+">>>"+floorStr+">>>"+floorNum+">>>"+orientation+">>>"+siteRef);
-
-                //CCUHsApi.getInstance().tagsDb.removeIdMap.values().remove(id);
+                CCUHsApi.getInstance().updateFloor(floor, floor.getId());
             }
-
         }
     }
 }

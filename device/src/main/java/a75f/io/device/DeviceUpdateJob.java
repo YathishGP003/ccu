1c95911264726775009e9da3acd0f61a5e7ae5f8
Samjith Sadasivan <samjith@75f.io>
2021-11-12 07:24:50 -0600

    Remove temp injection code

--- device/src/main/java/a75f/io/device/DeviceUpdateJob.java
+++ device/src/main/java/a75f/io/device/DeviceUpdateJob.java
@@ -77,49 +77,6 @@ public class DeviceUpdateJob extends BaseJob implements WatchdogMonitor
         } else {
             CcuLog.d(L.TAG_CCU_JOB, "DeviceUpdateJob <- Instance of job still running");
         }
-        feedTestData();
-    }
-    
-    
-    private void feedTestData() {
-        new Thread() {
-            @Override public void run() {
-                super.run();
-                ArrayList<HashMap> devices = CCUHsApi.getInstance().readAll("device");
-                Random rand = new Random();
-                for (HashMap deviceMap : devices) {
-                    if (deviceMap.containsKey("smartnode")) {
-                        RxTask.executeAsync(() -> {
-                            short addr = Short.parseShort(deviceMap.get("addr").toString());
-                            CcuLog.d(L.TAG_CCU_JOB, "DeviceUpdateJob sendTestData to node " + addr);
-                            CmToCcuOverUsbSnRegularUpdateMessage_t msg = new CmToCcuOverUsbSnRegularUpdateMessage_t();
-                            msg.update.smartNodeAddress.set(addr);
-                            msg.update.roomTemperature.set(650 + rand.nextInt(100));
-                            msg.update.rssi.set((byte) -100);
-                            SmartNodeSensorReading_t humidity = new SmartNodeSensorReading_t();
-                            humidity.sensorType.set(1);
-                            humidity.sensorData.set(20 + rand.nextInt(80));
-                            SmartNodeSensorReading_t co2 = new SmartNodeSensorReading_t();
-                            co2.sensorType.set(2);
-                            co2.sensorData.set(1000 + rand.nextInt(1000));
-                            SmartNodeSensorReading_t co = new SmartNodeSensorReading_t();
-                            co.sensorType.set(3);
-                            co.sensorData.set(200 + rand.nextInt(200));
-                            SmartNodeSensorReading_t no = new SmartNodeSensorReading_t();
-                            no.sensorType.set(4);
-                            no.sensorData.set(rand.nextInt(10));
-                            SmartNodeSensorReading_t[] sensors = new SmartNodeSensorReading_t[4];
-                            sensors[0] = humidity;
-                            sensors[1] = co2;
-                            sensors[2] = co;
-                            sensors[3] = no;
-                            msg.update.sensorReadings = sensors;
-                            Pulse.regularSNUpdate(msg);
-                        });
-                    }
-                }
-            }
-        }.start();
     }
         
     }

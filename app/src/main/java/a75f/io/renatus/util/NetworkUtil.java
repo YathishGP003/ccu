ea11ac4af06cc2ca3f6be52efef73929d4e92ad5
jtheertha <jtheertha@75f.io>
2022-01-20 10:15:58 +0000

    Merged PR 2926: CCU: Cloud Icon to show if CCU is Online or Offline
    Using url connection in place of ping, as ping is not working for silo url.
     Silo url is used in place of google as there could be restriction to use google in some countries.
    
    Related work items: #9753

--- app/src/main/java/a75f/io/renatus/util/NetworkUtil.java
+++ app/src/main/java/a75f/io/renatus/util/NetworkUtil.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class NetworkUtil {
     
     public static boolean isNetworkConnected(Context context) {
@@ -13,12 +16,20 @@ public class NetworkUtil {
     }
 
     public static boolean isConnectedToInternet(Context context) {
+        int responseCode = 0;
         if (isNetworkConnected(context)) {
+            HttpURLConnection urlc;
             try {
-                return Runtime.getRuntime().exec("ping -c 1 www.google.com").waitFor() == 0;
+                URL url = new URL("https://silo-playground.testbed-75f-service.com/v2/about");
+                urlc = (HttpURLConnection) url.openConnection();
+                urlc.setConnectTimeout(3000);
+                urlc.connect();
+                responseCode = urlc.getResponseCode();
             } catch (Exception e) {
                 e.printStackTrace();
-                return false;
+            }
+            finally {
+                return responseCode == 200;
             }
         } else
             return false;

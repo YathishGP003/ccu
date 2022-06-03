0e09919fcc16e7fc4367a8f4957fe693403d85b3
Spoorthi Dev <sdev@75f.io>
2022-06-03 13:50:04 +0530

    unwanted changes removed

--- app/src/main/java/a75f/io/renatus/schedules/ScheduleUtil.java
+++ app/src/main/java/a75f/io/renatus/schedules/ScheduleUtil.java
@@ -23,14 +23,9 @@ public class ScheduleUtil {
         for (String zoneId : spillsMap.keySet()) {
 
             Zone z = new Zone.Builder().setHashMap(CCUHsApi.getInstance().readMapById(zoneId)).build();
-
             HashMap<Object, Object> scheduleHashMap = CCUHsApi.getInstance().readEntity("schedule and roomRef " +
                     "== " +z.getId());
             Schedule zoneSchedule = CCUHsApi.getInstance().getScheduleById(scheduleHashMap.get("id").toString());
-
-            if(!zoneSchedule.isNamedSchedule())
-                zoneSchedule = CCUHsApi.getInstance().getScheduleById(z.getScheduleRef());
-
             ArrayList<Interval> spills = spillsMap.get(zoneId);
 
             Iterator daysIterator = zoneSchedule.getDays().iterator();

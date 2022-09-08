d2feb42c5083bb32ce1ee6f2c2c63484a18669b5
JayaTheertha <jtheertha@75f.io>
2022-09-08 15:56:31 +0530

    Fix for 13319

--- logic/src/main/java/a75f/io/logic/messaging/MessagingListener.java
+++ logic/src/main/java/a75f/io/logic/messaging/MessagingListener.java
@@ -82,10 +82,8 @@ public class MessagingListener implements ServerSentEvent.Listener {
 
     @Override
     public boolean onRetryError(ServerSentEvent sse, Throwable throwable, Response response) {
-        if( Globals.getInstance().isAckdMessagingEnabled() ) {
-            MessagingClient.getInstance().init();
-        }
         CcuLog.w(L.TAG_CCU_MESSAGING, "SSE connection error. Attempting to reconnect", throwable);
+        //We will simply request to retry here, assuming the error ie recoverable.
         return true;
     }
 

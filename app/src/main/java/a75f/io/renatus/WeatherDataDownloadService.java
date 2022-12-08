b458ec5294d4d961263ed14ca661ee86df9fbde6
Luke Gaskill <lgaskill@75f.io>
2022-12-08 10:03:54 -0600

    Remove decimal offset for humidity readings

--- app/src/main/java/a75f/io/renatus/WeatherDataDownloadService.java
+++ app/src/main/java/a75f/io/renatus/WeatherDataDownloadService.java
@@ -80,7 +80,7 @@ public class WeatherDataDownloadService {
 
                         mCurrentTemp = current.getDouble("airTemp");
                         mCurrentHumidity = CCUUtils.roundTo2Decimal(current.getDouble("humidity"));
-                        mOutsideAirEnthalpy = CCUUtils.calculateAirEnthalpy(mCurrentTemp, mCurrentHumidity / 100);
+                        mOutsideAirEnthalpy = CCUUtils.calculateAirEnthalpy(mCurrentTemp, mCurrentHumidity);
 
                         mSummary = StringUtils.capitalize(current.getString("description"));
                         micon = current.getString("icon");
@@ -112,7 +112,7 @@ public class WeatherDataDownloadService {
                     } else {
                         mCurrentTemp = PreferenceManager.getDefaultSharedPreferences(RenatusApp.getAppContext()).getFloat("outside_cur_temp", (float) mCurrentTemp);
                         mCurrentHumidity = PreferenceManager.getDefaultSharedPreferences(RenatusApp.getAppContext()).getFloat("outside_hum", (float) mCurrentHumidity);
-                        mOutsideAirEnthalpy = CCUUtils.calculateAirEnthalpy(mCurrentTemp, mCurrentHumidity / 100);
+                        mOutsideAirEnthalpy = CCUUtils.calculateAirEnthalpy(mCurrentTemp, mCurrentHumidity);
                         mPrecipIntensity = PreferenceManager.getDefaultSharedPreferences(RenatusApp.getAppContext()).getFloat("outside_precip", (float) mPrecipIntensity);
                     }
                 } catch (JSONException | NullPointerException e) {

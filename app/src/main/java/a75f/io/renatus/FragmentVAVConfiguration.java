b7ef610d84f57011fe324dfef1fab0b09f48e9e7
Bharathpanchakshari <33091312+Bharathpanchakshari@users.noreply.github.com>
2023-04-20 15:04:37 +0530

    migration added

--- app/src/main/java/a75f/io/renatus/FragmentVAVConfiguration.java
+++ app/src/main/java/a75f/io/renatus/FragmentVAVConfiguration.java
@@ -481,7 +481,6 @@ public class FragmentVAVConfiguration extends BaseDialogFragment implements Adap
             damperSize.setSelection(damperSizeAdapter.getPosition(String.valueOf(mProfileConfig.damperSize)), false);
             damperShape.setSelection(damperShapeAdapter.getPosition(DamperShape.values()[mProfileConfig.damperShape].displayName), false);
             reheatType.setSelection(reheatTypesAdapter.getPosition(ReheatType.values()[mProfileConfig.reheatType].displayName), false);
-//            enableOccupancyControl.setChecked(mProfileConfig.enableOccupancyControl);
             enableCO2Control.setChecked(mProfileConfig.enableCO2Control);
             enableIAQControl.setChecked(mProfileConfig.enableIAQControl);
             enableAutoAwayControl.setChecked(mProfileConfig.enableAutoAwayControl);
@@ -601,7 +600,6 @@ public class FragmentVAVConfiguration extends BaseDialogFragment implements Adap
         vavConfig.reheatType = reheatTypeSelected.ordinal();
         vavConfig.setNodeType(mNodeType);
         vavConfig.setNodeAddress(mSmartNodeAddress);
-//        vavConfig.enableOccupancyControl = enableOccupancyControl.isChecked();
         vavConfig.enableCO2Control = enableCO2Control.isChecked();
         vavConfig.enableIAQControl = enableIAQControl.isChecked();
         vavConfig.enableAutoAwayControl = enableAutoAwayControl.isChecked();
--- logic/src/main/java/a75f/io/logic/util/MigrationUtil.java
+++ logic/src/main/java/a75f/io/logic/util/MigrationUtil.java
@@ -354,6 +354,8 @@ public class MigrationUtil {
             updateKind(CCUHsApi.getInstance());
             PreferenceUtil.setKindCorrectionMigration();
         }
+        
+        migrateEnableOccupancyControl(CCUHsApi.getInstance());
 
         L.saveCCUState();
     }
@@ -1958,5 +1960,21 @@ public class MigrationUtil {
             hsApi.addPoint(DiagEquip.getDiagSafeModePoint(equipRef, equipDis, siteRef, tz));
 
         }
+    }
+
+    private static void migrateEnableOccupancyControl(CCUHsApi ccuHsApi) {
+
+        ArrayList<HashMap<Object, Object>> vavEquips = ccuHsApi.readAllEntities("vav and equip and zone");
+        for (HashMap<Object, Object> equip : vavEquips) {
+            ArrayList<HashMap<Object, Object>> enableOccupancyControlPoints = ccuHsApi.readAllEntities("enable and occupancy and control and equipRef == \"" + equip.get("id") + "\"");
+            if (!enableOccupancyControlPoints.isEmpty()) {
+                for (HashMap<Object, Object> enableOccupancyControlPoint : enableOccupancyControlPoints) {
+                    if (!enableOccupancyControlPoint.isEmpty()) {
+                        ccuHsApi.deleteEntity(enableOccupancyControlPoint.get("id").toString());
+                    }
+                }
+            }
+        }
+
     }
 }

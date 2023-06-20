f736a680eb1e5beb9f1534222ea0595f828ee086
kamal singh thakur <ksthakur@75f.com>
2023-06-20 14:29:45 +0530

    changes for bacnet, sub, unsub, poll apis, already approved change, migrating to new branch

--- haystack/src/main/java/a75f/io/api/haystack/AndroidHSClient.java
+++ haystack/src/main/java/a75f/io/api/haystack/AndroidHSClient.java
@@ -1,6 +1,10 @@
 package a75f.io.api.haystack;
 
 import org.projecthaystack.HGrid;
+import org.projecthaystack.HNum;
+import org.projecthaystack.HRef;
+import org.projecthaystack.HVal;
+import org.projecthaystack.HWatch;
 import org.projecthaystack.client.HClient;
 import org.projecthaystack.server.HOp;
 import org.projecthaystack.server.HServer;
@@ -53,6 +57,73 @@ public class AndroidHSClient extends HClient
     {
         return db;
     }
-    
+
+
+    public HGrid watchSubscribe(HGrid req) {
+        // check for watchId or watchId
+        String watchId = null;
+        String watchDis = null;
+        if (req.meta().has("watchId"))
+            watchId = req.meta().getStr("watchId");
+        else
+            watchDis = req.meta().getStr("watchDis");
+
+        // check for desired lease
+        HNum lease = null;
+        if (req.meta().has("lease"))
+            lease = (HNum) req.meta().get("lease");
+
+        // open or lookup watch
+        HWatch watch = watchId == null ?
+                watchOpen(watchDis, lease) :
+                watch(watchId);
+
+        // map grid to ids
+        HRef[] ids = gridToIds(req);
+
+        // subscribe and return resulting grid
+        return watch.sub(ids);
+    }
+
+    HRef[] gridToIds(HGrid grid) {
+        HRef[] ids = new HRef[grid.numRows()];
+        for (int i = 0; i < ids.length; ++i) {
+            HVal val = grid.row(i).get("id");
+            ids[i] = valToId(val);
+        }
+        return ids;
+    }
+
+    HRef valToId(HVal val) {
+        return (HRef) val;
+    }
+
+    public HGrid watchUnSubscribe(HGrid req) {
+        // lookup watch, silently ignore failure
+        String watchId = req.meta().getStr("watchId");
+        HWatch watch = watch(watchId, false);
+
+        // check for close or unsub
+        if (watch != null) {
+            if (req.meta().has("close"))
+                watch.close();
+            else
+                watch.unsub(gridToIds(req));
+        }
+
+        // nothing to return
+        return HGrid.EMPTY;
+    }
+
+    public HGrid watchPoll(HGrid req){
+        String watchId = req.meta().getStr("watchId");
+        HWatch watch = watch(watchId);
+
+        // poll cov or refresh
+        if (req.meta().has("refresh"))
+            return watch.pollRefresh();
+        else
+            return watch.pollChanges();
+    }
     
 }

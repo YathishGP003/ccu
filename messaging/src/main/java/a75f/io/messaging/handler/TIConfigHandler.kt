42c2ee10b89fdee57f299b8f000cc0f3d1cbebc8
Bharathpanchakshari <33091312+Bharathpanchakshari@users.noreply.github.com>
2023-07-07 18:32:09 +0530

    Bug 19447: CCU: Update to 637.2 reverted TI sensor option
    Handled upgrade scenario from internal portal
    
    Related bug - #19447

--- messaging/src/main/java/a75f/io/messaging/handler/TIConfigHandler.kt
+++ messaging/src/main/java/a75f/io/messaging/handler/TIConfigHandler.kt
@@ -17,80 +17,125 @@ class TIConfigHandler {
             val value = msgobject.get("val").asInt
             CCUHsApi.getInstance().writeHisValById(configPoint.id, value.toDouble())
 
-            val currentTemp: HashMap<Any, Any>? = haystack.readEntity("point and current and " +
-                    "temp and equipRef == \"" + configPoint.equipRef + "\"")
-            val nodeAddr = configPoint.group.toInt()
-
-            val equipHash = CCUHsApi.getInstance().readEntity(
-                "equip and group == \"$nodeAddr\""
+            val device: HashMap<Any, Any> = haystack.readEntity("cm and device ")
+            val currentTempPoint: HashMap<Any, Any> = haystack.readEntity(
+                "point and current and " +
+                        "temp and equipRef == \"" + configPoint.equipRef + "\""
             )
+            val nodeAddress = configPoint.group.toInt()
+
+            val supplyTempSensorTypePointId = haystack.readEntity("point and supply and " +
+                    "type and temp and equipRef == \"" + configPoint.equipRef + "\"")["id"].toString()
+
+            val roomTempSensorTypePointId = haystack.readEntity("point and space and " +
+                    "type and temp and equipRef == \"" + configPoint.equipRef + "\"")["id"].toString()
+
+            var supplyTempSensor = haystack.readDefaultValById(supplyTempSensorTypePointId).toInt()
+            var roomTempSensor = haystack.readDefaultValById(roomTempSensorTypePointId).toInt()
+
+            if (configPoint.id == supplyTempSensorTypePointId) {
+                supplyTempSensor = msgobject.get("val").asInt
+            }
+            if (configPoint.id == roomTempSensorTypePointId) {
+                roomTempSensor = msgobject.get("val").asInt
+            }
+
+            val roomTempSensorConfig = RoomTempSensor.values()[roomTempSensor]
+            val supplyTempSensorConfig = SupplyTempSensor.values()[supplyTempSensor]
+
+            if (device.isNotEmpty() && currentTempPoint.isNotEmpty()) {
+
+                ControlMote.setPointEnabled(nodeAddress, Port.TH1_IN.name, false)
+                ControlMote.setPointEnabled(nodeAddress, Port.TH2_IN.name, false)
+                ControlMote.setPointEnabled(nodeAddress, Port.SENSOR_RT.name, false)
+                ControlMote.setCMPointEnabled(Port.TH1_IN.name, false)
+                ControlMote.setCMPointEnabled(Port.TH2_IN.name, false)
+                if (supplyTempSensorConfig == SupplyTempSensor.THERMISTOR_1 || roomTempSensorConfig == RoomTempSensor.THERMISTOR_1) {
+                    ControlMote.setPointEnabled(nodeAddress, Port.TH1_IN.name, true)
+                    ControlMote.setCMPointEnabled(Port.TH1_IN.name, true)
+                }
+                if (supplyTempSensorConfig == SupplyTempSensor.THERMISTOR_2 || roomTempSensorConfig == RoomTempSensor.THERMISTOR_2) {
+                    ControlMote.setPointEnabled(nodeAddress, Port.TH2_IN.name, true)
+                    ControlMote.setCMPointEnabled(Port.TH2_IN.name, true)
+                }
+                if (roomTempSensorConfig == RoomTempSensor.SENSOR_BUS_TEMPERATURE) {
+                    ControlMote.setPointEnabled(nodeAddress, Port.SENSOR_RT.name, true)
+                }
+                updateSensorPoints(supplyTempSensorConfig, roomTempSensorConfig, currentTempPoint, nodeAddress, configPoint.equipRef)
+            }
+        }
+
+        private fun updateSensorPoints(
+            supplyTempSensor: SupplyTempSensor,
+            roomTempSensor: RoomTempSensor,
+            currentTempPoint: HashMap<Any, Any>,
+            nodeAddr: Int,
+            equipRef: String
+        ) {
+
+            val hayStack = CCUHsApi.getInstance()
+            val equipHash = CCUHsApi.getInstance().readEntity("equip and group == \"$nodeAddr\"")
             val equip = Equip.Builder().setHashMap(equipHash).build()
-
-            ControlMote.setPointEnabled(nodeAddr, Port.TH1_IN.name, false)
-            ControlMote.setPointEnabled(nodeAddr, Port.TH2_IN.name, false)
-            ControlMote.setPointEnabled(nodeAddr, Port.SENSOR_RT.name, false)
-
-            ControlMote.setCMPointEnabled(Port.TH1_IN.name, false)
-            ControlMote.setCMPointEnabled(Port.TH2_IN.name, false)
-
-            if (configPoint.markers.contains("space")) {
-                val supplyTempSensorTypePoint = haystack.readDefaultVal("point and supply and " +
-                        "type and temp and equipRef == \"" + configPoint.equipRef + "\"").toInt()
-
-                val roomTempSensorPoint: HashMap<Any, Any>? = haystack.readEntity(
-                    "point and space and not type and temp and equipRef == \"" + configPoint.equipRef + "\"")
-
-                if (value == SupplyTempSensor.THERMISTOR_1.ordinal || supplyTempSensorTypePoint == RoomTempSensor.THERMISTOR_1.ordinal) {
-                    ControlMote.setPointEnabled(nodeAddr, Port.TH1_IN.name, true)
-                    ControlMote.setCMPointEnabled(Port.TH1_IN.name, true)
-                    ControlMote.updatePhysicalPointRef(nodeAddr, Port.TH1_IN.name, roomTempSensorPoint?.get("id").toString())
+            val supplyTempSensorPoint = hayStack.readEntity(
+                "point and supply and not type and temp and equipRef == \"$equipRef\""
+            )
+            val roomTempSensorPoint = hayStack.readEntity(
+                "point and space and not type and temp and equipRef == \"$equipRef\""
+            )
+            val supplyTempSensorPointExists = supplyTempSensorPoint.isNotEmpty()
+            when (supplyTempSensor) {
+                SupplyTempSensor.NONE -> if (supplyTempSensorPointExists) {
+                    CCUHsApi.getInstance().deleteEntity(
+                        Objects.requireNonNull(
+                            supplyTempSensorPoint["id"]
+                        ).toString()
+                    )
                 }
-                if (value == SupplyTempSensor.THERMISTOR_2.ordinal || supplyTempSensorTypePoint == RoomTempSensor.THERMISTOR_2.ordinal) {
-                    ControlMote.setPointEnabled(nodeAddr, Port.TH2_IN.name, true)
-                    ControlMote.setCMPointEnabled(Port.TH2_IN.name, true)
-                    ControlMote.updatePhysicalPointRef(nodeAddr, Port.TH2_IN.name, roomTempSensorPoint?.get("id").toString())
+                SupplyTempSensor.THERMISTOR_1 -> if (!supplyTempSensorPointExists) {
+                    val supplyAirTempId = CazEquipUtil.createSupplyAirTempPoint(equip, nodeAddr)
+                    ControlMote.updatePhysicalPointRef(
+                        nodeAddr,
+                        Port.TH1_IN.name,
+                        Objects.requireNonNull(supplyAirTempId)
+                    )
+                } else {
+                    ControlMote.updatePhysicalPointRef(
+                        nodeAddr, Port.TH1_IN.name, Objects.requireNonNull(
+                            supplyTempSensorPoint["id"]
+                        ).toString()
+                    )
                 }
-                if (value == RoomTempSensor.SENSOR_BUS_TEMPERATURE.ordinal) {
-                    ControlMote.setPointEnabled(nodeAddr, Port.SENSOR_RT.name, true)
-                    ControlMote.updatePhysicalPointRef(nodeAddr, Port.SENSOR_RT.name, currentTemp?.get("id").toString())
+                SupplyTempSensor.THERMISTOR_2 -> if (!supplyTempSensorPointExists) {
+                    val supplyAirTempId = CazEquipUtil.createSupplyAirTempPoint(equip, nodeAddr)
+                    ControlMote.updatePhysicalPointRef(
+                        nodeAddr,
+                        Port.TH2_IN.name,
+                        Objects.requireNonNull(supplyAirTempId)
+                    )
+                } else {
+                    ControlMote.updatePhysicalPointRef(
+                        nodeAddr, Port.TH2_IN.name, Objects.requireNonNull(
+                            supplyTempSensorPoint["id"]
+                        ).toString()
+                    )
                 }
-
-            } else {
-                val roomTempSensorTypePoint = haystack.readDefaultVal("point and space and " +
-                        "type and temp and equipRef == \"" + configPoint.equipRef + "\"").toInt()
-
-                val supplyTempSensorPoint: HashMap<Any, Any>? = haystack.readEntity(
-                    "point and supply and not type and temp and equipRef == \"" + configPoint.equipRef + "\"")
-
-                if (value == SupplyTempSensor.THERMISTOR_1.ordinal || roomTempSensorTypePoint == RoomTempSensor.THERMISTOR_1.ordinal) {
-                    if (supplyTempSensorPoint?.isEmpty() == true) {
-                        val supplyAirTempId = CazEquipUtil.createSupplyAirTempPoint(equip, nodeAddr)
-                        ControlMote.updatePhysicalPointRef(
-                            nodeAddr,
-                            Port.TH1_IN.name,
-                            Objects.requireNonNull(supplyAirTempId)
-                        )
-                    } else {
-                        ControlMote.updatePhysicalPointRef(nodeAddr, Port.TH1_IN.name, supplyTempSensorPoint?.get("id").toString())
-                    }
-                    ControlMote.setPointEnabled(nodeAddr, Port.TH1_IN.name, true)
-                    ControlMote.setCMPointEnabled(Port.TH1_IN.name, true)
-                }
-                if (value == SupplyTempSensor.THERMISTOR_2.ordinal || roomTempSensorTypePoint == RoomTempSensor.THERMISTOR_2.ordinal) {
-                    if (supplyTempSensorPoint?.isEmpty() == true) {
-                        val supplyAirTempId = CazEquipUtil.createSupplyAirTempPoint(equip, nodeAddr)
-                        ControlMote.updatePhysicalPointRef(
-                            nodeAddr,
-                            Port.TH2_IN.name,
-                            Objects.requireNonNull(supplyAirTempId)
-                        )
-                    } else {
-                        ControlMote.updatePhysicalPointRef(nodeAddr, Port.TH2_IN.name, supplyTempSensorPoint?.get("id").toString())
-                    }
-                    ControlMote.setPointEnabled(nodeAddr, Port.TH2_IN.name, true)
-                    ControlMote.setCMPointEnabled(Port.TH2_IN.name, true)
-                }
-
+            }
+            when (roomTempSensor) {
+                RoomTempSensor.SENSOR_BUS_TEMPERATURE -> ControlMote.updatePhysicalPointRef(
+                    nodeAddr, Port.SENSOR_RT.name, Objects.requireNonNull(
+                        currentTempPoint["id"]
+                    ).toString()
+                )
+                RoomTempSensor.THERMISTOR_1 -> ControlMote.updatePhysicalPointRef(
+                    nodeAddr, Port.TH1_IN.name, Objects.requireNonNull(
+                        roomTempSensorPoint["id"]
+                    ).toString()
+                )
+                RoomTempSensor.THERMISTOR_2 -> ControlMote.updatePhysicalPointRef(
+                    nodeAddr, Port.TH2_IN.name, Objects.requireNonNull(
+                        roomTempSensorPoint["id"]
+                    ).toString()
+                )
             }
 
         }

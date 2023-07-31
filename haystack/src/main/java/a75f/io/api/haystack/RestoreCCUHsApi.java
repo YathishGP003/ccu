55274ef59a1bf559b665502e30bd08b6299b68b7
Bharathpanchakshari <33091312+Bharathpanchakshari@users.noreply.github.com>
2023-07-31 23:01:07 +0530

    SM Add Point: CCU Won't Consume Points Created by Site Manager within CCU Equips
     1. The add command sent by site manager is ignored by the CCU, hence it wont be added
     2. During replace we add a bew filter "not createdByApplication" to make sure we do not pull points created by the SM
     3. During unregister and re-register we do not pull point from the remote
    
     Related ticket - #18415

--- haystack/src/main/java/a75f/io/api/haystack/RestoreCCUHsApi.java
+++ haystack/src/main/java/a75f/io/api/haystack/RestoreCCUHsApi.java
@@ -311,7 +311,7 @@ public class RestoreCCUHsApi {
 
         HClient hClient = new HClient(ccuHsApi.getHSUrl(), HayStackConstants.USER, HayStackConstants.PASS);
         HDict ccuDict = new HDictBuilder().add("filter",
-                "point and equipRef == " + StringUtils.prependIfMissing(equipRow.get("id").toString()
+                "point and not createdByApplication and equipRef == " + StringUtils.prependIfMissing(equipRow.get("id").toString()
                         , "@")).toDict();
         HGrid pointsGrid = invokeWithRetry("read", hClient, HGridBuilder.dictToGrid(ccuDict), retryCountCallback);
         if(pointsGrid == null){

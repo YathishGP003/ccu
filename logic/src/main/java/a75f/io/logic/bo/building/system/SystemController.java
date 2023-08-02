04b36cf9aa15608dd8235d399c54f7c24dd88601
Samjith Sadasivan <samjith@75f.io>
2023-08-02 15:47:40 -0500

    DAB cumulative damper adjustment 1% algo

--- logic/src/main/java/a75f/io/logic/bo/building/system/SystemController.java
+++ logic/src/main/java/a75f/io/logic/bo/building/system/SystemController.java
@@ -7,6 +7,7 @@ import org.projecthaystack.UnknownRecException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
 
 import a75f.io.api.haystack.CCUHsApi;
 import a75f.io.api.haystack.Equip;
@@ -27,7 +28,7 @@ public abstract class SystemController
     public State systemState = OFF;
     public static final int MIN_DAMPER_FOR_CUMULATIVE_CALCULATION = 1;
     public boolean emergencyMode = false;
-    
+    public Set<String> deadZones = new HashSet<>();
     public abstract int getCoolingSignal() ;
     public abstract int getHeatingSignal();
     public abstract double getAverageSystemHumidity();

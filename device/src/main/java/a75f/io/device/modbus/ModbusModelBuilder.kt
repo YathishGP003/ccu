607594a0a33301ff1c64cf8efb8c986095d8ca1f
Manjunath Kundaragi <mkundaragi@75f.io>
2023-08-03 11:17:42 +0530

    Refactored ModbusModelBuilder.kt

--- device/src/main/java/a75f/io/device/modbus/ModbusModelBuilder.kt
+++ device/src/main/java/a75f/io/device/modbus/ModbusModelBuilder.kt
@@ -12,55 +12,74 @@ import a75f.io.api.haystack.modbus.LogicalPointTags
 import a75f.io.api.haystack.modbus.Parameter
 import a75f.io.api.haystack.modbus.Register
 import a75f.io.api.haystack.modbus.UserIntentPointTags
-import java.util.ArrayList
 
 /**
  * Created by Manjunath K on 31-07-2023.
  */
 
+const val ID = "id"
+const val PARAM_DEFINITION_TYPE = "parameterDefinitionType"
+const val WORD_ORDER = "wordOrder"
+const val MULTIPLIER = "multiplier"
+const val BIT_PARAM_RANGE = "bitParamRange"
+const val BIT_PARAM = "bitParam"
+const val DISPLAY_UI = "displayInUi"
+const val UNIT = "unit"
+const val HIS_INTERPOLATE = "hisInterpolate"
+const val MIN_VAL = "minVal"
+const val MAX_VAL = "maxVal"
+const val INCREMENTAL_VAL = "incrementVal"
+const val CELL = "cell"
 
+/**
+ * @param zoneRef
+ * returns EquipmentDevice list for a given zone ref
+ */
 fun buildModbusModel(zoneRef: String): List<EquipmentDevice> {
-    val equipListMap = CCUHsApi.getInstance().readAllEntities("equip and modbus and not equipRef and roomRef == \"$zoneRef\"")
+    val equipListMap = getParentEquipMapByZone(zoneRef)
     val equips = mutableListOf<EquipmentDevice>()
     equipListMap.forEach { equipMap ->
-        val equipDevice =  buildEquipModel(equipMap,null)
-        val childEquipMaps = CCUHsApi.getInstance().readAllEntities("equip and modbus and equipRef== \"${equipMap["id"].toString()}\"")
-        childEquipMaps.forEach {
-            equipDevice.equips.add(buildEquipModel(it, equipMap["id"].toString()))
-        }
+        val equipDevice = buildEquipModel(equipMap, null)
+        equipDevice.equips.addAll(getChildEquips(equipMap[ID].toString()))
         equips.add(equipDevice)
     }
     return equips
 }
 
+/**
+ * @param slaveId
+ * returns EquipmentDevice for give slave id
+ */
 fun buildModbusModel(slaveId: Int): EquipmentDevice {
-    val parentMap = CCUHsApi.getInstance().readEntity("equip and modbus and not equipRef and group == \"$slaveId\"")
-    val equipDevice =  buildEquipModel(parentMap,null)
-    val childEquipMaps = CCUHsApi.getInstance().readAllEntities("equip and modbus and equipRef== \"${parentMap["id"].toString()}\"")
-    childEquipMaps.forEach {
-        equipDevice.equips.add(buildEquipModel(it,parentMap["id"].toString()))
-    }
+    val parentMap = getParentEquipMapBySlaveId(slaveId)
+    val equipDevice = buildEquipModel(parentMap, null)
+    equipDevice.equips.addAll(getChildEquips(parentMap[ID].toString()))
     return equipDevice
 }
 
+/**
+ * @param equipRef
+ * returns EquipmentDevice for given equip ref
+ */
 fun buildModbusModelByEquipRef(equipRef: String): EquipmentDevice {
     val parentMap = CCUHsApi.getInstance().readMapById(equipRef)
-    val equipDevice =  buildEquipModel(parentMap,null)
-    val childEquipMaps = CCUHsApi.getInstance().readAllEntities("equip and modbus and equipRef== \"${parentMap["id"].toString()}\"")
-    childEquipMaps.forEach {
-        equipDevice.equips.add(buildEquipModel(it,parentMap["id"].toString()))
-    }
+    val equipDevice = buildEquipModel(parentMap, null)
+    equipDevice.equips.addAll(getChildEquips(parentMap[ID].toString()))
     return equipDevice
 }
 
-
-private fun buildEquipModel(parentMap: HashMap<Any, Any>, parentEquipRef: String?) : EquipmentDevice {
+/**
+ * @param equipMap
+ * @param parentEquipRef
+ * returns the EquipmentDevice object with all the details
+ */
+private fun getEquipByMap(equipMap: HashMap<Any, Any>, parentEquipRef: String?): EquipmentDevice {
     val equipDevice = EquipmentDevice()
-    val equip = Equip.Builder().setHashMap(parentMap).build()
-    equipDevice.id = 0L
+    val equip = Equip.Builder().setHashMap(equipMap).build()
     equipDevice.modbusEquipIdId = null
-    equipDevice.name = equip.displayName
     equipDevice.description = null
+    equipDevice.id = 0L
+    equipDevice.name = equip.displayName
     equipDevice.equipType = equip.equipType
     equipDevice.vendor = equip.vendor
     equipDevice.modelNumbers = mutableListOf<String>(equip.model)
@@ -75,91 +94,128 @@ private fun buildEquipModel(parentMap: HashMap<Any, Any>, parentEquipRef: String
     equipDevice.isPaired = (equipDevice.deviceEquipRef == null)
     equipDevice.slaveId = equip.group.toInt()
     equipDevice.deviceEquipRef = equip.id
-    val deviceId = CCUHsApi.getInstance()
-        .readEntity("device and modbus and equipRef == \"${equip.id}\"")[Tags.ID]
-
-    val registersMap = CCUHsApi.getInstance().readAllEntities(
-        "physical and point and deviceRef == \"${deviceId}\""
-    )
-
-    registersMap.forEach { rawMap ->
-        val rawPhysicalPoint = RawPoint.Builder().setHashMap(rawMap).build()
-        val logicalPointMap = CCUHsApi.getInstance().readMapById(rawPhysicalPoint.pointRef)
-
-        val logicalPoint = Point.Builder().setHashMap(logicalPointMap).build()
-        val register = Register()
-        register.registerNumber = rawPhysicalPoint.registerNumber
-        register.registerAddress = rawPhysicalPoint.registerAddress.toInt()
-        register.registerType = rawPhysicalPoint.registerType
-        register.parameterDefinitionType = if (rawMap.containsKey("parameterDefinitionType")) rawMap["parameterDefinitionType"].toString() else null
-        register.wordOrder = if (rawMap.containsKey("wordOrder")) rawMap["wordOrder"].toString() else null
-        register.multiplier = if (rawMap.containsKey("multiplier")) rawMap["multiplier"].toString() else null
-        val param = Parameter()
-        param.parameterId = rawPhysicalPoint.parameterId
-        param.name = rawPhysicalPoint.shortDis
-        param.startBit = rawPhysicalPoint.startBit.toInt()
-        param.endBit = rawPhysicalPoint.endBit.toInt()
-        param.bitParamRange = if (rawMap.containsKey("bitParamRange")) rawMap["bitParamRange"].toString() else null
-        param.bitParam =  if (rawMap.containsKey("bitParam")) strToDouble(rawMap["bitParam"].toString()) else 0
-        param.logicalPointTags = mutableListOf<LogicalPointTags>()
-        param.commands = mutableListOf<Command>()
-        param.userIntentPointTags = mutableListOf<UserIntentPointTags>()
-        param.conditions = mutableListOf<Condition>()
-        param.isDisplayInUI = logicalPoint.markers.contains("displayInUi")
-
-        val isWritable = logicalPoint.markers.contains("writable")
-        logicalPoint.markers.forEach { marker ->
-            param.logicalPointTags.add(getLogicalPointTags(marker,null))
-            if (isWritable)
-                param.userIntentPointTags.add(getUserIntentPointTags(marker,null))
-        }
-
-        if (!logicalPoint.unit.isNullOrEmpty()) {
-            param.logicalPointTags.add(getLogicalPointTags("unit",logicalPoint.unit))
-            if (isWritable)
-                param.userIntentPointTags.add(getUserIntentPointTags("unit",logicalPoint.unit))
-        }
-        if (!logicalPoint.hisInterpolate.isNullOrEmpty()) {
-            param.logicalPointTags.add(getLogicalPointTags("hisInterpolate", logicalPoint.hisInterpolate))
-            if (isWritable)
-                param.userIntentPointTags.add(getUserIntentPointTags("hisInterpolate", logicalPoint.hisInterpolate))
-        }
-
-        if (!logicalPoint.minVal.isNullOrEmpty()) {
-            param.logicalPointTags.add(getLogicalPointTags("minVal", logicalPoint.minVal))
-            if (isWritable)
-                param.userIntentPointTags.add(getUserIntentPointTags("minVal", logicalPoint.minVal))
-        }
-        if (!logicalPoint.maxVal.isNullOrEmpty()) {
-            param.logicalPointTags.add(getLogicalPointTags("maxVal", logicalPoint.maxVal))
-            if (isWritable)
-                param.userIntentPointTags.add(getUserIntentPointTags("maxVal", logicalPoint.maxVal))
-        }
-        if (!logicalPoint.incrementVal.isNullOrEmpty()) {
-            param.logicalPointTags.add(getLogicalPointTags("incrementVal", logicalPoint.incrementVal))
-            if (isWritable)
-                param.userIntentPointTags.add(getUserIntentPointTags("incrementVal", logicalPoint.incrementVal))
-        }
-
-        if (!logicalPoint.cell.isNullOrEmpty()) {
-            param.logicalPointTags.add(getLogicalPointTags("cell", logicalPoint.cell))
-            if (isWritable)
-                param.userIntentPointTags.add(getUserIntentPointTags("cell", logicalPoint.cell))
-        }
-        if (!logicalPoint.enums.isNullOrEmpty()) {
-            if (logicalPoint.markers.contains("cmd")) {
-                param.commands = getCommands(logicalPoint.enums)
-            } else {
-                param.conditions = getConditions(logicalPoint.enums)
-            }
-        }
-        register.parameters = mutableListOf(param)
-        equipDevice.registers.add(register)
-
-    }
     return equipDevice
 }
 
+/**
+ * @param parentEquipRef
+ * returns all the child equip details for the given parent ref
+ */
+private fun getChildEquips(parentEquipRef: String): List<EquipmentDevice> {
+    val devices = mutableListOf<EquipmentDevice>()
+    val childEquipMaps = getChildEquipMap(parentEquipRef)
+    childEquipMaps.forEach { devices.add(buildEquipModel(it, parentEquipRef)) }
+    return devices
+}
+
+
+private fun buildEquipModel(parentMap: HashMap<Any, Any>, parentEquipRef: String?): EquipmentDevice {
+    val equipId = parentMap[ID].toString()
+    val equipDevice = getEquipByMap(parentMap, parentEquipRef)
+    val registersMapList = getRegistersMap(equipId)
+    registersMapList.forEach { registerMap -> equipDevice.registers.add(getRegister(registerMap)) }
+    return equipDevice
+}
+
+/**
+ * @param equipId
+ * return all the register map list for the the equip
+ */
+private fun getRegistersMap(equipId: String): ArrayList<HashMap<Any, Any>> {
+    val hsApi = CCUHsApi.getInstance()
+    val deviceId = hsApi.readId("device and modbus and equipRef == \"$equipId\"")
+    return hsApi.readAllEntities("physical and point and deviceRef == \"$deviceId\"")
+}
+
+/**
+ * @param rawMap
+ * returns register object with all the register details
+ */
+private fun getRegister(rawMap: HashMap<Any, Any>): Register {
+    val physicalPoint = RawPoint.Builder().setHashMap(rawMap).build()
+    val register = Register()
+    register.registerNumber = physicalPoint.registerNumber
+    register.registerAddress = physicalPoint.registerAddress.toInt()
+    register.registerType = physicalPoint.registerType
+    register.parameterDefinitionType = getValue(rawMap, PARAM_DEFINITION_TYPE)
+    register.wordOrder = getValue(rawMap, WORD_ORDER)
+    register.multiplier = getValue(rawMap, MULTIPLIER)
+    register.parameters = mutableListOf(getParameter(physicalPoint,rawMap))
+    return register
+}
+
+
+/**
+ * @param physicalPoint
+ * @param rawMap
+ * returns Parameter object with all the parameter details
+ */
+private fun getParameter(physicalPoint: RawPoint, rawMap: HashMap<Any, Any>): Parameter {
+    val param = getBasicParamData(physicalPoint,rawMap)
+    val logicalPointMap = CCUHsApi.getInstance().readMapById(physicalPoint.pointRef)
+    val logicalPoint = Point.Builder().setHashMap(logicalPointMap).build()
+    param.isDisplayInUI = logicalPoint.markers.contains(DISPLAY_UI)
+    val isWritable = logicalPoint.markers.contains(Tags.WRITABLE)
+    logicalPoint.markers.forEach { marker ->
+        param.logicalPointTags.add(getLogicalPointTags(marker, null))
+        if (isWritable)
+            param.userIntentPointTags.add(getUserIntentPointTags(marker, null))
+    }
+    addTagValues(param,logicalPoint.unit,UNIT,isWritable)
+    addTagValues(param,logicalPoint.hisInterpolate,HIS_INTERPOLATE,isWritable)
+    addTagValues(param,logicalPoint.minVal,MIN_VAL,isWritable)
+    addTagValues(param,logicalPoint.maxVal, MAX_VAL,isWritable)
+    addTagValues(param,logicalPoint.incrementVal,INCREMENTAL_VAL,isWritable)
+    addTagValues(param,logicalPoint.cell, CELL,isWritable)
+    if (!logicalPoint.enums.isNullOrEmpty()) {
+        if (logicalPoint.markers.contains(Tags.CMD))
+            param.commands = getCommands(logicalPoint.enums)
+        else
+            param.conditions = getConditions(logicalPoint.enums)
+    }
+    return param
+}
+
+
+/**
+ * @param physicalPoint
+ * @param rawMap
+ * returns Parameter object with basic parameter details
+ */
+private fun getBasicParamData(physicalPoint: RawPoint, rawMap: HashMap<Any, Any>): Parameter {
+    val param = Parameter()
+    param.parameterId = physicalPoint.parameterId
+    param.name = physicalPoint.shortDis
+    param.startBit = physicalPoint.startBit.toInt()
+    param.endBit = physicalPoint.endBit.toInt()
+    param.bitParamRange = getValue(rawMap, BIT_PARAM_RANGE)
+    param.bitParam = if (rawMap.containsKey(BIT_PARAM)) strToDouble(rawMap[BIT_PARAM].toString()) else 0
+    param.logicalPointTags = mutableListOf<LogicalPointTags>()
+    param.userIntentPointTags = mutableListOf<UserIntentPointTags>()
+    return param
+}
+
+
+/**
+ * @param param  Parameter reference
+ * @param value
+ * @param key
+ * @param isWritable to push into user intent
+ * return
+ */
+private fun addTagValues(param: Parameter,value: String?, key: String, isWritable: Boolean) {
+    if (!value.isNullOrEmpty()) {
+        param.logicalPointTags.add(getLogicalPointTags(key, value))
+        if (isWritable)
+            param.userIntentPointTags.add(getUserIntentPointTags(key, value))
+    }
+}
+
+/**
+ * @param tag user intent tag
+ * @param value value for the tag
+ * returns LogicalPointTags object
+ */
 private fun getLogicalPointTags(tag: String?, value: String?): LogicalPointTags {
     val logicalTag = LogicalPointTags()
     if (!tag.isNullOrEmpty())
@@ -169,6 +225,11 @@ private fun getLogicalPointTags(tag: String?, value: String?): LogicalPointTags 
     return logicalTag
 }
 
+/**
+ * @param tag user intent tag
+ * @param value value for the tag
+ * returns UserIntentPointTags object
+ */
 private fun getUserIntentPointTags(tag: String?, value: String?): UserIntentPointTags {
     val userIntentTag = UserIntentPointTags()
     if (!tag.isNullOrEmpty())
@@ -178,6 +239,11 @@ private fun getUserIntentPointTags(tag: String?, value: String?): UserIntentPoin
     return userIntentTag
 }
 
+/**
+ * @param enum
+ * function takes enum as input and convert into commands
+ * return list of commands
+ */
 private fun getConditions(enum: String?): List<Condition> {
     val conditionsList = mutableListOf<Condition>()
     if (!enum.isNullOrEmpty()) {
@@ -197,6 +263,11 @@ private fun getConditions(enum: String?): List<Condition> {
     return conditionsList
 }
 
+/**
+ * @param enum
+ * Function takes enum convert into commands
+ * returns list of commands
+ */
 private fun getCommands(enum: String?): List<Command> {
     val commandsList = mutableListOf<Command>()
     if (!enum.isNullOrEmpty()) {
@@ -216,6 +287,9 @@ private fun getCommands(enum: String?): List<Command> {
     return commandsList
 }
 
+/**
+ * function if string contains dot . convert to double else return 0
+ */
 private fun strToDouble(str: String?): Int {
     if (str != null) {
         if (str.contains("."))
@@ -223,4 +297,41 @@ private fun strToDouble(str: String?): Int {
         return str.toInt()
     } else
         return 0
+}
+
+/**
+ * Function returns map value if key exist
+ */
+private fun getValue(map: HashMap<Any, Any>, key: String): String? {
+    return if(map.containsKey(key)) map[key].toString() else null
+}
+
+/**
+ * @param slaveId
+ * Read parent equip details by slave id
+ * return hashmap of equips
+ */
+private fun getParentEquipMapBySlaveId(slaveId: Int): HashMap<Any, Any> {
+    return CCUHsApi.getInstance()
+        .readEntity("equip and modbus and not equipRef and group == \"$slaveId\"")
+}
+
+/**
+ * @param zoneRef
+ * Read parent equip details by zone id
+ * return list of hashmaps for all the parent equips
+ */
+private fun getParentEquipMapByZone(zoneRef: String): ArrayList<HashMap<Any, Any>> {
+    return CCUHsApi.getInstance()
+        .readAllEntities("equip and modbus and not equipRef and roomRef == \"$zoneRef\"")
+}
+
+/**
+ * @param equipRef
+ * Read all the child equipments for the parent equip
+ * return list of child equip hashmaps for all selected parent map
+ */
+private fun getChildEquipMap(equipRef: String): ArrayList<HashMap<Any, Any>> {
+    return CCUHsApi.getInstance()
+        .readAllEntities("equip and modbus and equipRef== \"$equipRef\"")
 }

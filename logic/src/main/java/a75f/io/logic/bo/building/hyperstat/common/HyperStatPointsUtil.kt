c6eae3ddfd1d62dd721347d85591d2a6cfca5bb9
Bharathpanchakshari <33091312+Bharathpanchakshari@users.noreply.github.com>
2023-08-14 19:37:40 +0530

    CCU | Analog Out Control message not respecting the staged fan Voltage
    Related ticket - #21962

--- logic/src/main/java/a75f/io/logic/bo/building/hyperstat/common/HyperStatPointsUtil.kt
+++ logic/src/main/java/a75f/io/logic/bo/building/hyperstat/common/HyperStatPointsUtil.kt
@@ -1242,7 +1242,7 @@ class HyperStatPointsUtil(
                 configLogicalPointsList.add(
                     Triple(
                         pointData.first,
-                        Port.ANALOG_OUT_ONE,
+                        Port.ANALOG_OUT_TWO,
                         0.0
                     )
                 )
@@ -1298,7 +1298,7 @@ class HyperStatPointsUtil(
                 configLogicalPointsList.add(
                     Triple(
                         pointData.first,
-                        Port.ANALOG_OUT_ONE,
+                        Port.ANALOG_OUT_THREE,
                         0.0
                     )
                 )

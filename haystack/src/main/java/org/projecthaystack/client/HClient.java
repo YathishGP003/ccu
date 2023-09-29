ee6422c54e8e7a032cc9145474ecf77a9ec393c8
Jayatheertha <jtheertha@75f.io>
2023-09-29 12:00:34 +0000

    Merged PR 9447: fix for the bug 20412 and 19852
    fix for the bug 20412 and 19852
    
    Cherry picked from !9271
    
    Related work items: #19852, #20412

--- haystack/src/main/java/org/projecthaystack/client/HClient.java
+++ haystack/src/main/java/org/projecthaystack/client/HClient.java
@@ -38,6 +38,7 @@ import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
 import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -959,7 +960,10 @@ public class HClient extends HProj
           return s.toString();
         }
         }catch (Exception e){
-          if(e instanceof InterruptedIOException){
+          if(e instanceof SocketTimeoutException){
+            // don't throw exception. Do retry
+          }
+          else if(e instanceof InterruptedIOException){
             throw new NullHGridException(e.getMessage());
           }
           Log.i("CCU_REPLACE","Exception occurred while hitting "+uriStr);
@@ -971,7 +975,7 @@ public class HClient extends HProj
         retryCountCallback.onRetry(retry);
         isOKResponse = false;
 
-      }while(retry <= maxRetryCount);
+      }while(retry < maxRetryCount);
     }
     return null;
   }

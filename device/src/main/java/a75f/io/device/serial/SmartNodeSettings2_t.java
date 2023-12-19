c6afbc875bb805aaa085e85d60e436796e481792
nprill <nprill@75f.io>
2023-12-19 12:08:24 -0600

    - SN Settings2 message: changed damperShape and condensateSensor enums from 4-bit to 8-bit

--- device/src/main/java/a75f/io/device/serial/SmartNodeSettings2_t.java
+++ device/src/main/java/a75f/io/device/serial/SmartNodeSettings2_t.java
@@ -9,8 +9,8 @@ public class SmartNodeSettings2_t extends Struct {
     public final Unsigned16 maxCFMCooling = new Unsigned16(); ///< Ranges from 50-4000. Increments of 5. Default - 250
     public final Unsigned16 minCFMReheating = new Unsigned16(); ///< Ranges from 50-4000. Increments of 5. Default - 50
     public final Unsigned16 maxCFMReheating = new Unsigned16(); ///< Ranges from 50-4000. Increments of 5. Default - 250
-    public final Enum4<DamperShape_t> damperShape = new Enum4<>(DamperShape_t.values()); ///< Refer to DamperShape_t. Default - Round
-    public final Enum4<CondensateSensor_t> condensateSensor = new Enum4<>(CondensateSensor_t.values()); ///< Refer to CondensateSensor_t. Default - Normally Open
+    public final Enum8<DamperShape_t> damperShape = new Enum8<>(DamperShape_t.values()); ///< Refer to DamperShape_t. Default - Round
+    public final Enum8<CondensateSensor_t> condensateSensor = new Enum8<>(CondensateSensor_t.values()); ///< Refer to CondensateSensor_t. Default - Normally Open
     public final Unsigned8 damperSize = new Unsigned8(); ///< Value in Inches. Will be converted to Feet in Fail-safe. Nick: to confirm
     public final Unsigned16 airflowCFMProportionalRange = new Unsigned16(); ///< 0-1500. Increments of 10. Default - 200
     public final Unsigned8 airflowCFMProportionalKFactor = new Unsigned8(); ///< 1/100th of the value. Default - 50

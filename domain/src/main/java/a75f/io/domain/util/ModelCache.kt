ebb23b4dd0c9f58b645eb678b5f385bf9c3fd232
nprill <nprill@75f.io>
2024-02-16 16:24:42 -0600

    Load all VAV zone equip models at app startup

--- domain/src/main/java/a75f/io/domain/util/ModelCache.kt
+++ domain/src/main/java/a75f/io/domain/util/ModelCache.kt
@@ -20,11 +20,49 @@ object ModelCache {
         if (!hayStack.isCCURegistered) {
             CcuLog.i(Domain.LOG_TAG, "Load BuildingEquipModel")
             val buildingEquipModel = ResourceHelper.loadModel(BUILDING_EQUIP_MODEL, context)
-            modelContainer["657739fbd1743f797c4c2ca4"] = buildingEquipModel
+            modelContainer[MODEL_BUILDING_EQUIP] = buildingEquipModel
             CcuLog.i(Domain.LOG_TAG, "BuildingEquipModel loaded ${buildingEquipModel.name}")
         }
+
+        loadDeviceModels()
+        loadVavZoneEquipModels()
+
     }
 
+    private fun loadDeviceModels() {
+        modelContainer[MODEL_SMART_NODE_DEVICE] = getModelById(MODEL_SMART_NODE_DEVICE)
+        CcuLog.i(Domain.LOG_TAG, "smartNodeDevice loaded")
+
+        modelContainer[MODEL_HELIO_NODE_DEVICE] = getModelById(MODEL_HELIO_NODE_DEVICE)
+        CcuLog.i(Domain.LOG_TAG, "helioNodeDevice loaded")
+    }
+    
+    private fun loadVavZoneEquipModels() {
+        modelContainer[MODEL_SN_VAV_NO_FAN] = getModelById(MODEL_SN_VAV_NO_FAN)
+        CcuLog.i(Domain.LOG_TAG, "smartNodeVavReheatNoFan equip model loaded")
+
+        modelContainer[MODEL_SN_VAV_SERIES_FAN] = getModelById(MODEL_SN_VAV_SERIES_FAN)
+        CcuLog.i(Domain.LOG_TAG, "smartNodeVavReheatSeriesFan equip model loaded")
+
+        modelContainer[MODEL_SN_VAV_PARALLEL_FAN] = getModelById(MODEL_SN_VAV_PARALLEL_FAN)
+        CcuLog.i(Domain.LOG_TAG, "smartNodeVavReheatParallelFan equip model loaded")
+
+        modelContainer[MODEL_SN_VAV_ACB] = getModelById(MODEL_SN_VAV_ACB)
+        CcuLog.i(Domain.LOG_TAG, "smartNodeActiveChilledBeam equip model loaded")
+
+        modelContainer[MODEL_HN_VAV_NO_FAN] = getModelById(MODEL_HN_VAV_NO_FAN)
+        CcuLog.i(Domain.LOG_TAG, "helioNodeVavReheatNoFan equip model loaded")
+
+        modelContainer[MODEL_HN_VAV_SERIES_FAN] = getModelById(MODEL_HN_VAV_SERIES_FAN)
+        CcuLog.i(Domain.LOG_TAG, "helioNodeVavReheatSeriesFan equip model loaded")
+
+        modelContainer[MODEL_HN_VAV_PARALLEL_FAN] = getModelById(MODEL_HN_VAV_PARALLEL_FAN)
+        CcuLog.i(Domain.LOG_TAG, "helioNodeVavReheatParallelFan equip model loaded")
+
+        modelContainer[MODEL_HN_VAV_ACB] = getModelById(MODEL_HN_VAV_ACB)
+        CcuLog.i(Domain.LOG_TAG, "helioNodeActiveChilledBeam equip model loaded")
+    }
+    
     /**
      * Could directly used in Unit tests without calling init() to set the context.
      */

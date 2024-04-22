46c80ff87377cac243f39149335e243ea93219fd
Pramod Halliyavar <phalliyavar@75f.io>
2024-04-22 21:50:36 +0530

    Bug 28353: CCU | HS CPU | Analog Out Staged fan speed Configuration failure Updating the variable isStagedFanEnabled before used in RelayUis #28353

--- app/src/main/java/a75f/io/renatus/hyperstat/ui/HyperStatFragment.kt
+++ app/src/main/java/a75f/io/renatus/hyperstat/ui/HyperStatFragment.kt
@@ -675,6 +675,58 @@ class HyperStatFragment : BaseDialogFragment() {
 
         viewState.analogOutUis.forEachIndexed { index, analogOutState ->
             with(analogOutUIs[index]) {
+
+                if (viewModel is CpuViewModel) {
+                    if (analogOutState.enabled && analogOutState.association == CpuAnalogOutAssociation.PREDEFINED_FAN_SPEED.ordinal) {
+                        isStagedFanEnabled = true
+                    }
+                }
+
+            }
+        }
+
+        viewState.relays.forEachIndexed { index, relayState ->
+            with(relayUIs[index]) {
+                switch.isChecked = relayState.enabled
+                selector.isEnabled = relayState.enabled
+                selector.setSelection(relayState.association)
+
+                if (relayState.enabled) {
+                    when (relayState.association) {
+                        CpuRelayAssociation.COOLING_STAGE_1.ordinal -> isCoolingStage1Enabled = true
+                        CpuRelayAssociation.COOLING_STAGE_2.ordinal -> isCoolingStage2Enabled = true
+                        CpuRelayAssociation.COOLING_STAGE_3.ordinal -> isCoolingStage3Enabled = true
+                        CpuRelayAssociation.HEATING_STAGE_1.ordinal -> isHeatingStage1Enabled = true
+                        CpuRelayAssociation.HEATING_STAGE_2.ordinal -> isHeatingStage2Enabled = true
+                        CpuRelayAssociation.HEATING_STAGE_3.ordinal -> isHeatingStage3Enabled = true
+                    }
+                }
+                if (isViewModelCPUViewModel(viewModel)) {
+                    makeStagedFanVisible(
+                        isCoolingStage1Enabled,
+                        isCoolingStage2Enabled,
+                        isCoolingStage3Enabled,
+                        isHeatingStage1Enabled,
+                        isHeatingStage2Enabled,
+                        isHeatingStage3Enabled,
+                        isStagedFanEnabled
+                    )
+                } else {
+                    makeStagedFanVisible(
+                        isCoolingStage1Enabled = false,
+                        isCoolingStage2Enabled = false,
+                        isCoolingStage3Enabled = false,
+                        isHeatingStage1Enabled = false,
+                        isHeatingStage2Enabled = false,
+                        isHeatingStage3Enabled = false,
+                        false
+                    )
+                }
+            }
+        }
+
+        viewState.analogOutUis.forEachIndexed { index, analogOutState ->
+            with(analogOutUIs[index]) {
                 switch.isChecked = analogOutState.enabled
                 selector.isEnabled = analogOutState.enabled
                 selector.setSelection(analogOutState.association)
@@ -776,46 +828,6 @@ class HyperStatFragment : BaseDialogFragment() {
             llCO2Threshold.layoutParams = paramsLlCO2Threshold
             tvZoneCO2Threshold.layoutParams=paramsTvCO2Threshold
             zoneCO2Threshold.layoutParams=paramsCo2ThresholdSpinner
-        }
-
-        viewState.relays.forEachIndexed { index, relayState ->
-            with(relayUIs[index]) {
-                switch.isChecked = relayState.enabled
-                selector.isEnabled = relayState.enabled
-                selector.setSelection(relayState.association)
-
-                if (relayState.enabled) {
-                    when (relayState.association) {
-                        CpuRelayAssociation.COOLING_STAGE_1.ordinal -> isCoolingStage1Enabled = true
-                        CpuRelayAssociation.COOLING_STAGE_2.ordinal -> isCoolingStage2Enabled = true
-                        CpuRelayAssociation.COOLING_STAGE_3.ordinal -> isCoolingStage3Enabled = true
-                        CpuRelayAssociation.HEATING_STAGE_1.ordinal -> isHeatingStage1Enabled = true
-                        CpuRelayAssociation.HEATING_STAGE_2.ordinal -> isHeatingStage2Enabled = true
-                        CpuRelayAssociation.HEATING_STAGE_3.ordinal -> isHeatingStage3Enabled = true
-                    }
-                }
-                if (isViewModelCPUViewModel(viewModel)) {
-                    makeStagedFanVisible(
-                        isCoolingStage1Enabled,
-                        isCoolingStage2Enabled,
-                        isCoolingStage3Enabled,
-                        isHeatingStage1Enabled,
-                        isHeatingStage2Enabled,
-                        isHeatingStage3Enabled,
-                        isStagedFanEnabled
-                    )
-                } else {
-                    makeStagedFanVisible(
-                        isCoolingStage1Enabled = false,
-                        isCoolingStage2Enabled = false,
-                        isCoolingStage3Enabled = false,
-                        isHeatingStage1Enabled = false,
-                        isHeatingStage2Enabled = false,
-                        isHeatingStage3Enabled = false,
-                        false
-                    )
-                }
-            }
         }
 
         pendingAnalogOutChange = false

a90ec54d436b4bad1122a81d9a62afd6fb8a8fb1
Aditya Raj <araj@75f.io>
2024-05-29 11:52:18 +0000

    Merged PR 13622: 29130: Zone not having coloured border in heatmap - Master
    **Commit 1:** replaced duplicate CCUHsApi instance and its usage with the hayStack named CCUHsApi instance given as parameter in the MigrationHandler.kt class.  Also removed unnecessary semicolons.
    
    **Commit 2:** Modified the query to fetch system equip which is not modbus while setting the ahuRef upon creating TI profile. Added a migration code to correct the ahuRef in the TI profile which is executed on every version upgrade.
    
    **NOTE:** @<Manjunath Kundaragi>  Why is  handleSync() called at the end of isLevelCleanable() in MigrationHandler.kt?
    
    Related work items: #29130

--- logic/src/main/java/a75f/io/logic/bo/building/ccu/CazEquip.java
+++ logic/src/main/java/a75f/io/logic/bo/building/ccu/CazEquip.java
@@ -65,7 +65,7 @@ public class CazEquip
 
         Schedule schedule = UtilKt.getSchedule(roomRef,floorRef);
 
-        HashMap systemEquip = CCUHsApi.getInstance().read("equip and system");
+        HashMap systemEquip = CCUHsApi.getInstance().read("equip and system and not modbus");
         if (systemEquip != null && systemEquip.size() > 0) {
             ahuRef = systemEquip.get("id").toString();
         }

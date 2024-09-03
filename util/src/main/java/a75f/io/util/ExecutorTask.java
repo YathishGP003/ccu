833d3981638018553a8a188e8e250c4ac0e314c1
Samjith Sadasivan <samjith@75f.io>
2024-09-03 07:27:57 -0500

    New util module Define Coroutine and Executor Tasks

---
+++ util/src/main/java/a75f/io/util/ExecutorTask.java
@@ -0,0 +1,33 @@
+package a75f.io.util;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ExecutorTask {
+    private static final int MAXIMUM_POOL_SIZE = 4;
+    private static final ExecutorService service = Executors.newFixedThreadPool(MAXIMUM_POOL_SIZE);
+    private static final Handler mainHandler = new Handler(Looper.getMainLooper());
+
+    public static Future executeBackground(Runnable runnable) {
+        return service.submit(runnable);
+    }
+
+    /**
+     * Execute a task in background and post the result on main thread.
+     * @param preExecute - runs on the calling thread
+     * @param background - runs on background thread
+     * @param postExecute - runs on main thread
+     */
+    public static Future executeAsync(Runnable preExecute, Runnable background, Runnable postExecute) {
+        preExecute.run();
+        return service.submit(() -> {
+            background.run();
+            mainHandler.post(postExecute);
+            //service.shutdown();
+        });
+    }
+}

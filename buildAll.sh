04f00133003a73291a6cb24b2939e37bf47f56bb
samjith <samjith@75f.io>
2024-10-29 04:19:03 +0000

    Merged PR 16972: Version bump 2.18.3
    Version bump 2.18.3
    
    Related work items: #33215

--- buildAll.sh
+++ buildAll.sh
@@ -1,23 +1,28 @@
 ./gradlew clean
+sleep 5
 ./gradlew assembleQA
 cp app/build/outputs/apk/qa/*.apk .
 
+sleep 5
 ./gradlew clean
+sleep 5
 ./gradlew assembleStaging
 cp app/build/outputs/apk/staging/*.apk .
 
+sleep 5
 ./gradlew clean
+sleep 5
 ./gradlew assembleProd
 cp app/build/outputs/apk/prod/*.apk .
 
+sleep 5
 ./gradlew clean
-./gradlew assembleDaikin_prod
-cp app/build/outputs/apk/daikin_prod/*.apk .
-
-./gradlew clean
+sleep 5
 ./gradlew assembleCarrier_prod
 cp app/build/outputs/apk/carrier_prod/*.apk .
 
+sleep 5
 ./gradlew clean
+sleep 5
 ./gradlew assembleAiroverse_prod
 cp app/build/outputs/apk/airoverse_prod/*.apk .
